1bd9ced69489cf4f941f997999cb31b009ec17c5
Angel <gutierrezramosjoseangel@gmail.com>
2021-01-26 15:39:42 -0600

    Keyframes animations

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { ListOfCategories } from './components/ListOfCategories';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards';
 import { Logo }  from './components/Logo';
 
-import { GlobalStyle } from './GlobalStyles';
+import { GlobalStyle } from './styles/GlobalStyles';
 
 export const App = () => {
     return (
