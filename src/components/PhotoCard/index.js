a061888e71086dedf5d7cdd078187f48405fedc1
Angel <gutierrezramosjoseangel@gmail.com>
2021-01-29 00:00:34 -0600

    Use polyfill and dinamics imports

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -9,16 +9,20 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMG }) => {
     const [show, setShow] = useState(false);
 
     useEffect(() => {
-        const observer = new window.IntersectionObserver((entries) => {
-            const { isIntersecting } = entries[0];
-            if (isIntersecting){
-                setShow(true);
-                observer.disconnect();
-            }
-        });
-
-        observer.observe(element.current);
-
+        Promise.resolve(
+            typeof window.IntersectionObserver !== 'undefined'
+                ? window.IntersectionObserver
+                : import('intersection-observer')
+        ).then(() => {
+                const observer = new window.IntersectionObserver((entries) => {
+                    const { isIntersecting } = entries[0];
+                    if (isIntersecting){
+                        setShow(true);
+                        observer.disconnect();
+                    }
+                });
+                observer.observe(element.current);
+            })
     }, [element]);
 
     return (
@@ -36,7 +40,6 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMG }) => {
                     </Button>
                 </>
             }
-            
         </Article>
     )
 }
