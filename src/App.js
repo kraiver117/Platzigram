34c53fa00cccdcc8d4ccbc9de8d7b1855f4a60a9
Angel <gutierrezramosjoseangel@gmail.com>
2021-01-29 18:24:24 -0600

    Using render props to obtain a photo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { ListOfCategories } from './components/ListOfCategories';
 import { ListOfPhotoCards } from './container/ListOfPhotoCards';
 import { Logo }  from './components/Logo';
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery';
 
 import { GlobalStyle } from './styles/GlobalStyles';
 
 export const App = () => {
+    const urlParams = new window.URLSearchParams(window.location.search);
+    const detailId = urlParams.get('detail');
+
     return (
         <>
-            <Logo />
             <GlobalStyle />
-            <ListOfCategories />
-            <ListOfPhotoCards categoryId = {1}/>
+            <Logo />
+            {
+                detailId
+                    ?   <PhotoCardWithQuery id={detailId} />
+                    :   <>
+                            <ListOfCategories />
+                            <ListOfPhotoCards categoryId = {1}/> 
+                        </>
+            }
+            
         </>
     )
 }
