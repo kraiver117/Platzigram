bd4cc31d3ee6c21196eb41a6735fa370ba9d2c4a
Angel <gutierrezramosjoseangel@gmail.com>
2021-01-30 15:30:03 -0600

    Add Details page with Reach Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React from 'react';
-import { Logo }  from './components/Logo';
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery';
-import { Home } from './pages/Home';
-import { Router } from '@reach/router';
-
+import { Logo } from './components/Logo';
 import { GlobalStyle } from './styles/GlobalStyles';
 
-export const App = () => {
-    const urlParams = new window.URLSearchParams(window.location.search);
-    const detailId = urlParams.get('detail');
+import { Home } from './pages/Home';
+import { Router } from '@reach/router';
+import { Detail } from './pages/Detail';
 
+export const App = () => {
     return (
         <>
             <GlobalStyle />
             <Logo />
-            {
-                detailId
-                    ?   <PhotoCardWithQuery id={detailId} />
-                    :   <Router>
-                            <Home path='/' />
-                            <Home path='/pet/:categoryId' />
-                        </Router>
-            }
-            
+            <Router>
+                <Home path='/' />
+                <Home path='/pet/:categoryId' />
+                <Detail path='/detail/:detailId' />
+            </Router>
         </>
     )
 }
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -5,6 +5,7 @@ import { useNearScreen } from '../../hooks/useNearScreen';
 
 import  { FavButton } from '../FavButton';
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation';
+import { Link } from '@reach/router';
 
 const DEFAULT_IMG = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
 
@@ -18,11 +19,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMG }) => {
             {
                 show && 
                 <> 
-                    <a href={`/?detail=${id}`}>
-                    <ImgWrapper>
-                        <Img src={src} alt="" />
-                    </ImgWrapper>
-                    </a>
+                    <Link to={`/detail/${id}`}>
+                        <ImgWrapper>
+                            <Img src={src} alt="" />
+                        </ImgWrapper>
+                    </Link>
                     <ToggleLikeMutation>
                         {
                             (toggleLike) => {
